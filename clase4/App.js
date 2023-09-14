ec55f6271e12bf2074d4d8e99aeb2fa4fe8d9f8a
Jose <leivajd@gmail.com>
2023-09-13 19:26:12 -0600

    Clase 4

---
+++ clase4/App.js
@@ -0,0 +1,35 @@
+const Pet = (props) => {
+  console.log(props)
+  // return React.createElement('div', 
+  //   {}, 
+  //   React.createElement("h1", {}, props.name),
+  //   React.createElement("h2", {}, props.animal),
+  //   React.createElement("h3", {}, props.breed)
+  // )
+  return (
+    <div id="wrapper">
+      <h1>{props.name}</h1>
+      <h2>{props.animal}</h2>
+      <h3>{props.breed}</h3>
+    </div>
+  )
+}
+
+const App = () => {
+  // return React.createElement("div", {}, [
+  //   React.createElement("h1", {}, "Adopt Me!"),
+  //   React.createElement(Pet,{name: 'Bella', animal: 'dog', breed: 'Bulldog'}),
+  //   React.createElement(Pet,{name: 'Mimi', animal: 'cat', breed: 'black'})
+  // ]);
+  return (
+    <div>
+      <h1>Adopt Me!</h1>
+      <Pet name="Bella" animal="dog" breed="Bulldog"/>
+      <Pet name="Mimi" animal="cat" breed="black"/>
+    </div>
+  )
+};
+
+const container = document.getElementById("root");
+const root = ReactDOM.createRoot(container);
+root.render(React.createElement(App));
